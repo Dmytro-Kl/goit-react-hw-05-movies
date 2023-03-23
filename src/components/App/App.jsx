987d681b703cdf826621b2b2e428650ea6ebfd58
Dmytro-Kl <110299885+Dmytro-Kl@users.noreply.github.com>
2023-03-23 21:22:17 +0200

    hw_5_movies

---
+++ src/components/App/App.jsx
@@ -0,0 +1,34 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from './App.styled';
+import { Toaster } from 'react-hot-toast';
+import Navigation from 'components/Navigation';
+import { GlobalStyle } from 'GlobalStyle';
+
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Cast = lazy(() => import('components/Cast'));
+const Reviews = lazy(() => import('components/Reviews'));
+
+const App = () => {
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+      <GlobalStyle />
+      <Toaster toastOptions={{ duration: 2500 }} />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import * as API from '../../services/api';
+import { useEffect, useState } from 'react';
+import MoviesList from 'components/MoviesList';
+import Loader from 'components/Loader';
+// import { toast } from 'react-hot-toast';
+import Error from 'components/Error';
+
+function Home() {
+  const [movies, setMovies] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setStatus('pending');
+    API.getTrendingMovies()
+      .then(data => {
+        setStatus('resolved');
+        setMovies(data);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Tranding movies</h1>
+      {status === 'pending' && <Loader />}
+      {status === 'resolved' && <MoviesList movies={movies} />}
+      {status === 'rejected' && <Error error={error.message} />}
+    </div>
+  );
+}
+
+export default Home;
